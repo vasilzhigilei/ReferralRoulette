3b7c9b814da5782269d9675e023ad72562e87755
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-12-22 11:55:39 -0500

    created models

--- referralroulette/models.py
+++ referralroulette/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from colorfield.fields import ColorField
 
 class ServiceModel(models.Model):
-    
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100, default="")
+    prefix = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images",
+                              default="/static/referralroulette/missing.png")
+    color = ColorField(default='#F5F5F5')
+    text_color = ColorField(default='#212529')
+    tags = TaggableManager()
+    clicks = models.IntegerField()
+    active = models.BooleanField()
+
+class ReferralModel(models.Model):
+    link = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    clicks = models.IntegerField()
