0ae790377a8fac3c14a9d233a5ad3e575e96cc78
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-12-22 22:33:39 -0500

    templating work

--- referralroulette/models.py
+++ referralroulette/models.py
@@ -4,15 +4,19 @@ from colorfield.fields import ColorField
 
 class ServiceModel(models.Model):
     name = models.CharField(max_length=30)
+    display_name = models.CharField(max_length=30)
     description = models.CharField(max_length=100, default="")
     prefix = models.CharField(max_length=100)
     image = models.ImageField(upload_to="service_images",
-                              default="/static/referralroulette/missing.png")
+                              default="/service_images/missing.png")
     background_color = ColorField(default='#F5F5F5')
     text_color = ColorField(default='#212529')
     tags = TaggableManager()
     clicks = models.IntegerField(default=0)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 class ReferralModel(models.Model):
     link = models.CharField(max_length=100)
