4e4b4a73b0bc6f9b58ad38a5bfa617b326bbe373
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-12-25 21:34:57 -0500

    select2 redirect on select

--- referralroulette/static/searchbox.js
+++ referralroulette/static/searchbox.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-    $('.js-example-basic-single').select2({
+    $('#searchbox').select2({
         theme: "bootstrap4",
         placeholder: 'Search here...',
         allowClear: true,
     });
     $('b[role="presentation"]').hide();
+    $('#searchbox').on('select2:select', function(e) {
+        window.location.href = '../for/' + e.params.data['id'];
+    });
 });
