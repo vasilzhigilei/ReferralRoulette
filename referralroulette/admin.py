5bc3d50eb3e9a7291a9d92b32dddae1385f04358
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-12-30 19:54:44 -0500

    background with feathered edges

--- referralroulette/admin.py
+++ referralroulette/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ServiceModel
-from .models import ReferralModel
+from .models import ServiceModel, ReferralModel, CategoryModel
 
 @admin.register(ServiceModel)
 class ServiceAdmin(admin.ModelAdmin):
@@ -12,8 +11,16 @@ class ServiceAdmin(admin.ModelAdmin):
 class ReferralAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('service',)}
 
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+# unregister
 admin.site.unregister(ServiceModel)
 admin.site.unregister(ReferralModel)
+admin.site.unregister(CategoryModel)
+
 # import into admin
 admin.site.register(ServiceModel, ServiceAdmin)
-admin.site.register(ReferralModel)
+admin.site.register(ReferralModel, ReferralAdmin)
+admin.site.register(CategoryModel, CategoryAdmin)
