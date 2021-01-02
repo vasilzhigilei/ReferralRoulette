c7ef5572302a5cffae604850b66aa9aa6339d16a
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-01-02 12:46:47 -0500

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('REFERRALROULETTE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
