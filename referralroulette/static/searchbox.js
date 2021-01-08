3e0bf624ca4ceb6acd901fa8f015153c9dd9e645
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-01-07 23:40:31 -0500

    finally got placeholder work around with adapter

--- referralroulette/static/searchbox.js
+++ referralroulette/static/searchbox.js
@@ -3,21 +3,30 @@ $(document).ready(function() {
         "select2/utils",
         "select2/selection/single",
         "select2/selection/eventRelay",
-        "select2/dropdown/search"
+        "select2/dropdown/search",
+        "select2/selection/placeholder",
       ],
-      function (Utils, SingleSelection, EventRelay, DropdownSearch) {
-        var adapter = Utils.Decorate(SingleSelection, DropdownSearch);
+      function (Utils, SingleSelection, EventRelay, DropdownSearch, Placeholder) {
+        var adapter = Utils.Decorate(SingleSelection, DropdownSearch, Placeholder);
         adapter = Utils.Decorate(adapter, EventRelay);
       
-        adapter.prototype.render = function () {
-          var $rendered = DropdownSearch.prototype.render.call(this, SingleSelection.prototype.render);
-      
-          this.$searchContainer.hide();
-          this.$element.siblings('.select2').find('.selection').prepend(this.$searchContainer);
-      
-          return $rendered;
+        adapter.prototype.render = function (data) {
+            var $rendered = DropdownSearch.prototype.render.call(this, SingleSelection.prototype.render);
+        
+            this.$searchContainer.hide();
+            this.$element.siblings('.select2').find('.selection').prepend(this.$searchContainer);
+            
+            $rendered.empty().append(
+                "<div style='width: 100%; padding: 4px; color: gray;'>" +
+                (this.options.get("placeholder") || "")
+                + "</div>"
+                
+                );
+                
+                $('input.select2-search__field').prop('placeholder', 'Search here...');
+            return $rendered;
         };
-      
+
         var bindOrigin = adapter.prototype.bind;
         adapter.prototype.bind = function (container) {
           var self = this;
@@ -56,6 +65,7 @@ $(document).ready(function() {
     $('#searchbox').select2({
         theme: "bootstrap4",
         placeholder: 'Search here...',
+        searchInputPlaceholder: 'Search state...',
         allowClear: true,
         selectionAdapter: $.fn.select2.amd.require("SearchableSingleSelection"),
         dropdownAdapter: $.fn.select2.amd.require("UnsearchableDropdown")
