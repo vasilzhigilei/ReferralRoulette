2eddbadb61523ba5a41b16f6bc752769a4d1ab7a
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-01-09 19:18:10 -0500

    tabs

--- referralroulette/models.py
+++ referralroulette/models.py
@@ -6,6 +6,8 @@ class ServiceModel(models.Model):
     name = models.CharField(max_length=30)
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=100, default="")
+    company_description = models.CharField(max_length=3000, default="")
+    referral_description = models.CharField(max_length=3000, default="")
     prefix = models.CharField(max_length=100)
     image = models.ImageField(upload_to="service_images",
                               default="/service_images/missing.png")
