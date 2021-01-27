75a271d02b38ec1e560717422c861bd507245998
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-01-27 12:21:09 -0500

    addded AZURE_CUSTOM_DOMAIN

--- referralroulette/custom_azure.py
+++ referralroulette/custom_azure.py
@@ -6,9 +6,11 @@ class AzureMediaStorage(AzureStorage):
     account_key = os.getenv('REFERRALROULETTE_AZURE_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
+    AZURE_CUSTOM_DOMAIN = 'cdn.referralroulette.com'
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'djangostorage' # Must be replaced by your storage_account_name
     account_key = os.getenv('REFERRALROULETTE_AZURE_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
+    AZURE_CUSTOM_DOMAIN = 'cdn.referralroulette.com'
