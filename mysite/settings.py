d11d28997f41258cb59887f104c36c782cc86da5
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-01-27 11:58:31 -0500

    disable middleware temporarily

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,7 +112,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'mysite.middleware.CanonicalDomainMiddleware',
+    #'mysite.middleware.CanonicalDomainMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
